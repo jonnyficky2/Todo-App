dc8dafa9fbc39441f039789117a9b1e565165f31
jonnyficky2 <jonnyficky2@gmail.com>
2026-05-19 16:58:26 +0700

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,18 @@
+const quotes = [
+
+{
+  text: "Disiplin lebih penting daripada motivasi.",
+  author: "Daily Tracker"
+},
+
+{
+  text: "Sedikit progress tetap progress.",
+  author: "Unknown"
+},
+
+{
+  text: "Konsisten mengalahkan bakat.",
+  author: "Daily Tracker"
+}
+
+];
