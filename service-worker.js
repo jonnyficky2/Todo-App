89193390d1d7483349ef47649f2cae1bb5cfd040
jonnyficky2 <jonnyficky2@gmail.com>
2026-05-19 16:57:45 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "daily-tracker-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./chart.umd.js"
+];
+
+self.addEventListener(
+  "install",
+  (event) => {
+
+    event.waitUntil(
+
+      caches.open(CACHE_NAME)
+      .then(cache => {
+
+        return cache.addAll(
+          urlsToCache
+        );
+      })
+    );
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  (event) => {
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      ).then(response => {
+
+        return (
+          response ||
+          fetch(event.request)
+        );
+      })
+    );
+  }
+);
